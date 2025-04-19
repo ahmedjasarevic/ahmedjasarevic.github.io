d580e44b8df3dcd98fb97cb26d2d3ef1235a52ce
Ahmed Jasarevic <66922656+ahmedjasarevic@users.noreply.github.com>
2025-04-19 17:06:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from "./components";
 
 const App = () => {
+  useEffect(() => {
+    import('https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bundle.es.js')
+      .then(({ createChat }) => {
+        createChat({
+          webhookUrl: 'https://n8n-62tp.onrender.com/webhook/e137dfbb-3366-4952-a287-dfa789ff6bbf/chat',
+          initialMessages: [
+            "Hello! 👋",
+            "My name is Ahmed. How can I help you today?"
+          ]
+        });
+      });
+  }, []);
+
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
