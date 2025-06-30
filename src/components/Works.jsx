98ec3d12b608b95178d89fadde1ae9fe24744c08
Ahmed Jasarevic <66922656+ahmedjasarevic@users.noreply.github.com>
2025-06-30 18:23:36 +0200

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,7 +8,6 @@ import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link, website_link }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt options={{ max: 45, scale: 1, speed: 450 }} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
         <div className="relative w-full h-[230px]">
           <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl" />
@@ -39,24 +38,24 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link, 
           ))}
         </div>
       </Tilt>
-    </motion.div>
+  
   );
 };
 
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      
         <p className={styles.sectionSubText}>My work</p>
         <h2 className={styles.sectionHeadText}>Projects </h2>
-      </motion.div>
+    
 
       <div className="w-full flex">
-        <motion.p
+        <p
           variants={fadeIn("", "", 0.1, 1)} className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Explore a range of projects showcasing my skills in web development and design. From dynamic web applications to interactive user interfaces, each project demonstrates my commitment to delivering innovative and functional solutions tailored to meet specific needs. Dive into the details and see how I've turned concepts into impactful digital experiences.
-        </motion.p>
+        </p>
       </div>
 
       <div className="mt-20 flex flex-wrap gap-7">
